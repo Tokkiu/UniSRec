ec5f5167b1a9711da1829f54bb0e7dd04c74fb99
Tokkiu <mrgao.ary@gmail.com>
2023-03-13 15:41:40 +0800

    add vis

--- unisrec.py
+++ unisrec.py
@@ -94,6 +94,7 @@ class UniSRec(SASRec):
         self.cal_popular()
         self.epoch = 0
         self.vis = config['vis']
+        self.prefix = config['exp']
 
     def cal_curr_pop(self):
         pop = sum(self.pop_label)/10/len(self.pop_label)
